c45882171ff07c15e3b9c245748632f510d98e21
LiuNian666 <liunianbest@163.com>
2018-04-17 21:13:08 +0800

    django配置

--- myblog/settings.py
+++ myblog/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.blog',
+    'blog',
     'django_summernote',
 ]
 
@@ -71,7 +71,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'mybolg.wsgi.application'
+WSGI_APPLICATION = 'myblog.wsgi.application'
 
 
 # Database
